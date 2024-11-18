4de580a375b58014f8e583dea528da15fc8a9195
Martin <martinconcetti97@gmail.com>
2024-11-18 03:30:10 -0300

    Pause menu

---
+++ TP06_ConcettiMartin/Assets/Scrip/Menu/GameplayMenuController.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameplayMenuController : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+    private void Start()
+    {
+        AudioManager.Instance.PlayMusic("Main music");
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
+
+    private void Pause()
+    {
+        if (Time.timeScale == 1)
+        {
+            pauseMenu.SetActive(true);
+            SetTimeToZero();
+        }
+        else
+        {
+            ResumeGameplay();
+        }
+    }
+
+    private void GameOver()
+    {
+        SetTimeToZero();
+       // gameOverMenu.SetActive(true);
+
+    }
+    private void WinGame()
+    {
+        SetTimeToZero();
+        //winMenu.SetActive(true);
+    }
+
+    public void ChangeScene(string name)
+    {
+        SetTimeToOne();
+        SceneManager.LoadScene(sceneName: name);
+    }
+
+    public void ResumeGameplay()
+    {
+        pauseMenu.SetActive(false);
+        SetTimeToOne();
+    }
+    private void SetTimeToZero()
+    {
+        Time.timeScale = 0f;
+    }
+
+    private void SetTimeToOne()
+    {
+        Time.timeScale = 1f;
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+
+    public void Click()
+    {
+        AudioManager.Instance.PlayEffect("Click");
+
+    }
+}
