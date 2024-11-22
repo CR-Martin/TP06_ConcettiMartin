17a4a07e115b5e914bd366db07411c5338b2161c
Martin <martinconcetti97@gmail.com>
2024-11-22 17:45:41 -0300

    Settings can be open from game

--- TP06_ConcettiMartin/Assets/Scrip/Menu/GameplayMenuController.cs
+++ TP06_ConcettiMartin/Assets/Scrip/Menu/GameplayMenuController.cs
@@ -2,10 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameplayMenuController : MonoBehaviour
 {
+    [Header("Sliders")]
+    [SerializeField] private Slider musicVolume;
+    [SerializeField] private Slider sfxVolume;
+    [SerializeField] private Slider uiVolume;
+
+    [Header("Player prefs")]
+    [SerializeField] private string musicVolumePref = "musicVolume";
+    [SerializeField] private string effectVolumePref = "effectVolume";
+    [SerializeField] private string uiVolumePref = "uiVolume";
+
     [SerializeField] GameObject pauseMenu;
+
+    private void Awake()
+    {
+        if (!PlayerPrefs.HasKey(musicVolumePref) && !PlayerPrefs.HasKey(effectVolumePref) && !PlayerPrefs.HasKey(uiVolumePref))
+        {
+            PlayerPrefs.SetFloat(musicVolumePref, 1);
+            PlayerPrefs.SetFloat(effectVolumePref, 1);
+            PlayerPrefs.SetFloat(uiVolumePref, 1);
+            musicVolume.value = 1;
+            sfxVolume.value = 1;
+            uiVolume.value = 1;
+
+        }
+        else
+        {
+            musicVolume.value = PlayerPrefs.GetFloat(musicVolumePref);
+            sfxVolume.value = PlayerPrefs.GetFloat(effectVolumePref);
+            uiVolume.value = PlayerPrefs.GetFloat(uiVolumePref);
+
+        }
+
+        musicVolume.onValueChanged.AddListener(SetMusicVolume);
+        sfxVolume.onValueChanged.AddListener(SetSFXVolume);
+        uiVolume.onValueChanged.AddListener(SetUIVolume);
+    }
+
+    private void Start()
+    {
+        AudioManager.Instance.PlayMusic("Main music");
+        AudioManager.Instance.MusicVolume(PlayerPrefs.GetFloat(musicVolumePref));
+        AudioManager.Instance.SfxVolume(PlayerPrefs.GetFloat(effectVolumePref));
+        AudioManager.Instance.UIVolume(PlayerPrefs.GetFloat(uiVolumePref));
+
+    }
 
     void Update()
     {
@@ -46,6 +91,16 @@ public class GameplayMenuController : MonoBehaviour
         SceneManager.LoadScene(sceneName: name);
     }
 
+    public void ActivatePanel(GameObject panel)
+    {
+        panel.SetActive(true);
+    }
+
+    public void DeActivatePanel(GameObject panel)
+    {
+        panel.SetActive(false);
+    }
+
     public void ResumeGameplay()
     {
         pauseMenu.SetActive(false);
@@ -64,6 +119,24 @@ public class GameplayMenuController : MonoBehaviour
     public void CloseGame()
     {
         Application.Quit();
+    }
+
+    public void SetMusicVolume(float value)
+    {
+        PlayerPrefs.SetFloat(musicVolumePref, value);
+        AudioManager.Instance.MusicVolume(PlayerPrefs.GetFloat(musicVolumePref));
+    }
+
+    public void SetSFXVolume(float value)
+    {
+        PlayerPrefs.SetFloat(effectVolumePref, value);
+        AudioManager.Instance.SfxVolume(PlayerPrefs.GetFloat(effectVolumePref));
+    }
+
+    public void SetUIVolume(float value)
+    {
+        PlayerPrefs.SetFloat(uiVolumePref, value);
+        AudioManager.Instance.UIVolume(PlayerPrefs.GetFloat(uiVolumePref));
     }
 
     public void Click()
